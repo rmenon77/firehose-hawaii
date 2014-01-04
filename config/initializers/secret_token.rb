c36f5b958ebb3a508e62ae3c2fb1f5282f5c5fc1
Ruby <ruby@dhcp-168-105-87-80.wireless.manoa.hawaii.edu>
2014-01-04 13:37:02 -1000

    adding some copy

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirehoseApp::Application.config.secret_key_base = '44709f189d85661ad7d0007f116e45bbeb9fd39419ce7505fa68afe4a76f3b8fc5353390d7b224d7702bcc88c1dca2e91441475beb4d3cbd80fe005ac611d959'
